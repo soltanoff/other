94d4d7df1b78d7540c5c3d0d1498093db8e184d1
Ilya <piccadillable@gmail.com>
2016-04-22 23:12:28 +0300

    Added new scripts

--- SQL/MS SQL/Lab 7/Delete_Doctor.sql
+++ SQL/MS SQL/Lab 7/Delete_Doctor.sql
@@ -1,41 +1,34 @@
-/*
-alter table Doctor_Specialty add unique(id_doc, id_spec)
-go
-*/
-/* Процедура удаления специалиста */
-create procedure Delete_Specialist
+/* 7) Каскадное удаление врача с БД */
+create procedure Delete_Doctor
 	@name varchar(15),
 	@surname varchar(15),
 	@middle_name varchar(15),
-	@bday date,
-	@specialty varchar(30)
+	@bday date
 as
 	declare @id_doc int
-	declare @id_spec int
 
 	if @middle_name is null
 		set @id_doc = (select id_doc from Doctor where Doc_Name = @name and Doc_Surname = @surname and Doc_Middle_name is null and Doc_Bday_date = @bday)
 	else 
 		set @id_doc = (select id_doc from Doctor where Doc_Name = @name and Doc_Surname = @surname and Doc_Middle_name = @middle_name and Doc_Bday_date = @bday)
 
-	set @id_spec = (select id_spec from Specialty where Spec_Name = @specialty)
-
-	if (@id_spec is not null and @id_doc is not null)
+	if (@id_doc is not null)
 	begin
-		if (exists (select * from Doctor_Specialty where id_spec = @id_spec and id_doc = @id_doc))
-		begin
-			delete Doctor_Specialty where id_doc = @id_doc and id_spec = @id_spec
+		create table #doc_spec (id_doc_spec int)
 
-			if (not exists (select * from Doctor_Specialty where id_spec = @id_spec))
-				delete Specialty where id_spec = @id_spec
+		insert into #doc_spec
+			select id_doc_spec from Doctor_Specialty where id_doc = @id_doc
 
-			if (not exists (select * from Doctor_Specialty where id_doc = @id_doc))
-				delete Doctor where id_doc = @id_doc
-		end
+		delete Reception_History where exists (select * from #doc_spec where #doc_spec.id_doc_spec = Reception_History.id_doc_spec) 
+
+		delete Doctor_Specialty where id_doc = @id_doc
+
+		delete Doctor where id_doc = @id_doc
 	end
 go
 
-/* ======================================================================== */
+
+delete Patient
 delete Doctor
 delete Specialty
 /* ======================================================================== */
@@ -51,6 +44,8 @@ exec Add_Specialist 'Test2', 'Testov2', null, '21.05.1922', 'Test1'
 go
 exec Delete_Specialist 'Test2', 'Testov2', null, '21.05.1922', 'Test1'
 go
+exec Delete_Doctor 'Test', 'Testov', null, '21.05.1922'
+go
 /* ======================================================================== */
 select
 	Doc_Name, Doc_Surname, Doc_Middle_name, Spec_Name 
@@ -63,6 +58,9 @@ select * from Doctor_Specialty
 select * from Specialty
 select * from Patient
 select * from Reception_History
+
+delete Reception_History
+
 /* ======================================================================== */
 insert into Patient values
 	(0, 'Qw', 'ER', 'Ty', '21.01.1488'),
@@ -70,6 +68,7 @@ insert into Patient values
 	(2, 'Zx', 'Cv', 'BN', '21.01.1488')
 
 insert into Reception_History values
-	(0, 0, 1, '21.01.2016'),
-	(1, 1, 2, '21.01.2016'),
-	(2, 2, 3, '21.01.2016')
+	(0, 0, 1, '21.01.2016 11:00'),
+	(1, 1, 2, '21.01.2016 12:00'),
+	(2, 2, 3, '21.01.2016 13:00'),
+	(3, 2, 0, '21.01.2016 10:00')
