8198c500f571c809b16fafd9d8dc45c1a718d333
Ilya <piccadillable@gmail.com>
2016-10-23 17:52:51 +0300

    Added resource iteraction

--- Python/TaskManager/config.py
+++ Python/TaskManager/config.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
+import os
+
 VERSION = 'v0.0.1'
 
+
 TASK_PRIORITY = [
-    u'Высокий',
-    u'Средний',
-    u'Низкий'
+    u'Высокий',     # 1
+    u'Средний',     # 2
+    u'Низкий'       # 3
 ]
 
 TASK_STATUS = [
-    'born',
-    'ready',
-    'execute',
-    'waiting',
-    'finish'
+    'born',         # 0
+    'ready',        # 1
+    'execute',      # 2
+    'waiting',      # 3
+    'finish'        # 4
 ]
 
 DEFAULT_TASK_NAME = u'Процесс #%s'
+
+RESOURCES_PATH = os.getcwd() + '\\resources'
+RESOURCES_LIST = os.listdir(RESOURCES_PATH)
