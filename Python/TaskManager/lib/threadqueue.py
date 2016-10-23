f2ad04763a3723ababab9008e4ac753f467e7d3b
Ilya <piccadillable@gmail.com>
2016-10-23 18:01:13 +0300

    Refactoring and remastering SJF & CThreadQueue
    Remove logic elements from CThreadQueue & added all planning logic to
    SJF

--- Python/TaskManager/lib/threadqueue.py
+++ Python/TaskManager/lib/threadqueue.py
@@ -1,19 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
+from config import TASK_STATUS
 
 
 class CThreadQueue(object):
+    u"""
+    item of queue:
+    {
+        'thread': thread,               Thread
+        'name': name,                   str
+        'eventStart': eStart,           Event
+        'eventFinish': eFinish,         Event
+        'priority': priority,           int
+        'status': status,               int
+        'time': time,                   int
+        'resources': []                 list
+    }
+    """
     def __init__(self, queue=list(dict())):
-        self._queue = queue
+        self.data = queue
 
     def push(self, x):
-        self._queue.append(x)
+        self.data.append(x)
 
     def pop(self):
-        return self._queue.pop(0)
+        if self.data and self.data[0]['status'] != 4:
+            return self.data[0]
+        else:
+            return None
+
+    # def sort(self):
+    #    self.data.sort(key=lambda k: (k['status'], k['priority'], k['time']))
+    #    self.changeStatus()
+
+    # def changeStatus(self, pos=1):
+    #    for x in self.data[pos:]:
+    #        if x['status'] != 4:
+    #            x['status'] = 3
 
     def __len__(self):
-        return len(self._queue)
-
-    def getList(self):
-        return self._queue
+        return len(self.data)
--- Python/TaskManager/logic/planning.py
+++ Python/TaskManager/logic/planning.py
@@ -2,23 +2,24 @@
 # -*- coding: utf8 -*-
 from threading import Event, Thread
 
-from config import TASK_STATUS
+# from config import TASK_STATUS
 from lib.threadqueue import CThreadQueue
 
 
-def createProcess(name, target, priority=1, time=4):
+def createProcess(name, target, priority=1, time=4, resources=[]):
     eStart = Event()
     eFinish = Event()
     thread = Thread(target=target, args=(name, eStart, eFinish, time))
 
     return {
-        'thread': thread,
-        'name': name,
-        'eventStart': eStart,
-        'eventFinish': eFinish,
-        'priority': priority,
-        'status': TASK_STATUS[0],
-        'time': time
+        'thread':       thread,
+        'name':         name,
+        'eventStart':   eStart,
+        'eventFinish':  eFinish,
+        'priority':     priority,
+        'status':       1,          # ready
+        'time':         time,
+        'resources':    resources   # list of id's of available resources
     }
 
 
@@ -26,37 +27,72 @@ class SJF(object):
     def __init__(self):
         self.queue = CThreadQueue()
         self.finishedThreads = []
-        self.allThreads = []
+        # self.allThreads = []
+
         self.sjfThread = Thread(target=self.sjfRoutine)
+        self.sjfFinishThreadEvent = Event()
+        self.sjfNewThreadEvent = Event()
+        self.sjfFinishThreadEvent.clear()
+        self.sjfNewThreadEvent.clear()
+
         self.currentThread = None
         self.suspended = False
+        self.enable = True
+
+    # TODO: 1) execute planning with available resources of all thread
+    # TODO: 2) write selecting and changing priority level
+    # TODO: 3) write timer, whos run a little process in query
+    def startPlanning(self):
+        self.queue.data.sort(key=lambda k: (k['status'], k['priority'], k['time']))
+        self.changeStatus()
+        self.queue.data.sort(key=lambda k: (k['status'], k['priority'], k['time']))
+
+    def changeStatus(self, pos=1):
+        for x in self.queue.data[pos:]:
+            if x['status'] != 4:
+                x['status'] = 3
 
     def sjfRoutine(self):
-        while True:
-            if self.suspended or \
-                    (not len(self.queue.getList())) or \
-                    (self.currentThread and self.currentThread['eventStart'].is_set()):
-                continue
-            elif self.currentThread:
-                self.finishedThreads.append(self.currentThread)
+        while self.enable:
+            self.sjfFinishThreadEvent.wait()    # wait current executed thread
+            if self.currentThread:
+                self.currentThread['eventFinish'].wait()
+                self.currentThread['status'] = 4    # set finish status
+                self.startPlanning()
+                # self.startPlanning()
 
-            self.currentThread = self.queue.pop()
-            self.currentThread['thread'].start()
+            self.currentThread = self.queue.pop()   # can return None
 
-            if not self.suspended:
-                self.currentThread['eventStart'].set()
+            if self.currentThread:
+                self.currentThread['status'] = 2
+                self.currentThread['thread'].start()
 
-            self.currentThread['eventFinish'].clear()
+                if not self.suspended:
+                    self.currentThread['eventStart'].set()
+
+                self.currentThread['eventFinish'].clear()
+            else:
+                self.sjfNewThreadEvent.clear()  # all threads in queue are finished
+                self.sjfNewThreadEvent.wait()   # wait a new thread in queue
+
+        # if self.currentThread and self.currentThread['thread'].isAlive():
+        #    self.currentThread['thread'].terminate()
+        print u'[TaskManager] Closing...'
 
     def suspendCurrentThread(self):
         self.suspended = True
+        self.sjfFinishThreadEvent.clear()
         if self.currentThread and self.currentThread['thread'].isAlive():
             self.currentThread['eventStart'].clear()
+            self.currentThread['status'] = 1    # ready
 
     def resumeCurrentThread(self):
         self.suspended = False
+        self.sjfFinishThreadEvent.set()
+        self.sjfNewThreadEvent.set()
         if self.currentThread and self.currentThread['thread'].isAlive():
             self.currentThread['eventStart'].set()
+            self.currentThread['status'] = 2    # execute
 
     def start(self):
         self.sjfThread.start()
