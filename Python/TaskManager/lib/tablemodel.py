bfce0f3deeb7ab116acbcc95960d7e01f932b2e3
Ilya <piccadillable@gmail.com>
2016-10-26 00:21:16 +0300

    Added new column, added text color on status

--- Python/TaskManager/lib/tablemodel.py
+++ Python/TaskManager/lib/tablemodel.py
@@ -2,14 +2,14 @@
 # -*- coding: utf8 -*-
 from PyQt4 import QtCore, QtGui
 
-from config import TASK_PRIORITY, TASK_STATUS
+from config import TASK_PRIORITY, TASK_STATUS, DEFAULT_PRIORITY_COLOR
 
 
 class CTableModel(QtCore.QAbstractTableModel):
     def __init__(self, parent, body=list()):
         QtCore.QAbstractTableModel.__init__(self)
         self.parent = parent
-        self.header = [u'Имя процесса', u'Приоритет', u'Статус', u'Время выполнения']
+        self.header = [u'Имя процесса', u'Процессор', u'Приоритет', u'Статус', u'Время выполнения']
         self.body = body
 
     def rowCount(self, parent=None):
@@ -24,15 +24,23 @@ class CTableModel(QtCore.QAbstractTableModel):
         if role == QtCore.Qt.FontRole:
             row = index.row()
             col = index.column()
-            if col == 2 and self.body[row]['status'] == 2:
-                font = QtGui.QFont()
-                font.setBold(True)
-                font.setItalic(True)
-                return QtCore.QVariant(font)
-            if col == 2 and self.body[row]['status'] == 3:
-                font = QtGui.QFont()
-                font.setBold(True)
-                return QtCore.QVariant(font)
+            if col == 3:
+                if self.body[row]['status'] == 2:
+                    font = QtGui.QFont()
+                    font.setBold(True)
+                    font.setItalic(True)
+                    return QtCore.QVariant(font)
+                elif self.body[row]['status'] == 3:
+                    font = QtGui.QFont()
+                    font.setBold(True)
+                    return QtCore.QVariant(font)
+        elif role == QtCore.Qt.TextColorRole:
+            row = index.row()
+            col = index.column()
+            if self.body[row]['status'] == 4:
+                return QtCore.QVariant(QtGui.QColor(QtCore.Qt.lightGray))
+            elif col == 2:
+                return QtCore.QVariant(DEFAULT_PRIORITY_COLOR[self.body[row]['priority']])
 
         if not index.isValid():
             return QtCore.QVariant()
@@ -45,10 +53,12 @@ class CTableModel(QtCore.QAbstractTableModel):
             if col == 0:
                 value = self.body[row]['name']
             elif col == 1:
-                value = TASK_PRIORITY[self.body[row]['priority']]
+                value = self.body[row]['processor']
             elif col == 2:
-                value = TASK_STATUS[self.body[row]['status']]
+                value = TASK_PRIORITY[self.body[row]['priority']]
             elif col == 3:
+                value = TASK_STATUS[self.body[row]['status']]
+            elif col == 4:
                 value = self.body[row]['time']
 
         return QtCore.QVariant(value)
