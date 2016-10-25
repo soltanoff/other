d8ea40699de448d102db3f1bdbd241bd1b8d6a01
Ilya <piccadillable@gmail.com>
2016-10-26 00:17:55 +0300

    Change programm version, added new params

--- Python/TaskManager/config.py
+++ Python/TaskManager/config.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 import os
+from PyQt4 import QtGui, QtCore
 
-VERSION = 'v0.0.1'
+VERSION = 'v0.5.1'
 
 
 TASK_PRIORITY = [
-    u'Высокий',     # 1
-    u'Средний',     # 2
-    u'Низкий'       # 3
+    u'Высокий',     # 0
+    u'Средний',     # 1
+    u'Низкий'       # 2
 ]
 
 TASK_STATUS = [
@@ -20,6 +21,18 @@ TASK_STATUS = [
 ]
 
 DEFAULT_TASK_NAME = u'Процесс #%s'
+DEFAULT_PROC_NAME = u'Процессор #%s'
+DEFAULT_PRIORITY_COLOR =[
+    QtGui.QColor(QtCore.Qt.red),        # 0
+    QtGui.QColor(QtCore.Qt.blue),       # 1
+    QtGui.QColor(QtCore.Qt.darkGreen)   # 2
+]
 
-RESOURCES_PATH = os.getcwd() + '\\resources'
-RESOURCES_LIST = os.listdir(RESOURCES_PATH)
+try:
+    RESOURCES_PATH = os.getcwd() + '\\resources'
+    RESOURCES_LIST = os.listdir(RESOURCES_PATH)
+except Exception as e:
+    print '[CFG_ERROR] %s' % e.msg if hasattr(e, 'msg') else e.message
+    RESOURCES_PATH = ''
+    RESOURCES_LIST = ''
+
