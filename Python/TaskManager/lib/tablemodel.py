b85e5fbea427b4ebcd34531c39976090b389bdac
Ilya <piccadillable@gmail.com>
2016-10-30 02:11:04 +0300

    fix exception of list walkin

--- Python/TaskManager/lib/tablemodel.py
+++ Python/TaskManager/lib/tablemodel.py
@@ -20,48 +20,49 @@ class CTableModel(QtCore.QAbstractTableModel):
 
     def data(self, index, role):
         self.layoutChanged.emit()
+        try:
+            if role == QtCore.Qt.FontRole:
+                row = index.row()
+                col = index.column()
+                if col == 3:
+                    if self.body[row].status == 2:
+                        font = QtGui.QFont()
+                        font.setBold(True)
+                        font.setItalic(True)
+                        return QtCore.QVariant(font)
+                    elif self.body[row].status == 3:
+                        font = QtGui.QFont()
+                        font.setBold(True)
+                        return QtCore.QVariant(font)
+            elif role == QtCore.Qt.TextColorRole:
+                row = index.row()
+                col = index.column()
+                if self.body[row].status == 4:
+                    return QtCore.QVariant(QtGui.QColor(QtCore.Qt.lightGray))
+                elif col == 2:
+                    return QtCore.QVariant(DEFAULT_PRIORITY_COLOR[self.body[row].priority])
 
-        if role == QtCore.Qt.FontRole:
-            row = index.row()
-            col = index.column()
-            if col == 3:
-                if self.body[row]['status'] == 2:
-                    font = QtGui.QFont()
-                    font.setBold(True)
-                    font.setItalic(True)
-                    return QtCore.QVariant(font)
-                elif self.body[row]['status'] == 3:
-                    font = QtGui.QFont()
-                    font.setBold(True)
-                    return QtCore.QVariant(font)
-        elif role == QtCore.Qt.TextColorRole:
-            row = index.row()
-            col = index.column()
-            if self.body[row]['status'] == 4:
-                return QtCore.QVariant(QtGui.QColor(QtCore.Qt.lightGray))
-            elif col == 2:
-                return QtCore.QVariant(DEFAULT_PRIORITY_COLOR[self.body[row]['priority']])
-
-        if not index.isValid():
+            if not index.isValid():
+                return QtCore.QVariant()
+            elif role != QtCore.Qt.DisplayRole and role != QtCore.Qt.EditRole:
+                return QtCore.QVariant()
+            value = ''
+            if role == QtCore.Qt.DisplayRole:
+                row = index.row()
+                col = index.column()
+                if col == 0:
+                    value = self.body[row].name
+                elif col == 1:
+                    value = self.body[row].processor
+                elif col == 2:
+                    value = TASK_PRIORITY[self.body[row].priority]
+                elif col == 3:
+                    value = TASK_STATUS[self.body[row].status]
+                elif col == 4:
+                    value = self.body[row].time
+            return QtCore.QVariant(value)
+        except:
             return QtCore.QVariant()
-        elif role != QtCore.Qt.DisplayRole and role != QtCore.Qt.EditRole:
-            return QtCore.QVariant()
-        value = ''
-        if role == QtCore.Qt.DisplayRole:
-            row = index.row()
-            col = index.column()
-            if col == 0:
-                value = self.body[row]['name']
-            elif col == 1:
-                value = self.body[row]['processor']
-            elif col == 2:
-                value = TASK_PRIORITY[self.body[row]['priority']]
-            elif col == 3:
-                value = TASK_STATUS[self.body[row]['status']]
-            elif col == 4:
-                value = self.body[row]['time']
-
-        return QtCore.QVariant(value)
 
     def headerData(self, section, orientation, role):
         if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
