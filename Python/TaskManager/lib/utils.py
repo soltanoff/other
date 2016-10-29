94afb09873f7e6c3a789d987f1ffe7ee4a7d6c12
Ilya <piccadillable@gmail.com>
2016-10-30 02:10:05 +0300

    fix utils structure

--- Python/TaskManager/lib/utils.py
+++ Python/TaskManager/lib/utils.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
+from threading import Event, Thread
+
 from PyQt4 import QtGui
 
 from config import RESOURCES_LIST
+from logic.processing import Process
 
 
 def setResources(lar, lur, process=list()):
@@ -11,15 +14,27 @@ def setResources(lar, lur, process=list()):
         lar.addItems(RESOURCES_LIST)
 
     lur.clear()
-    # parentItem = QtGui.QTreeWidgetItem('P')
-    # self.addTopLevelItem(parentItem)
-    # childItem = QtGui.QTreeWidgetItem('C')
-    # parentItem.insertChild(0, childItem)
     for x in process:
         parentItem = QtGui.QTreeWidgetItem()
-        parentItem.setText(0, x['name'])
+        parentItem.setText(0, x.name)
         lur.insertTopLevelItem(0, parentItem)
-        for x in x['resources']:
+        for x in x.resources:
             childItem = QtGui.QTreeWidgetItem()
             childItem.setText(0, x)
             parentItem.insertChild(0, childItem)
+
+
+def createProcess(name, target, priority=1, time=4, resources=list()):
+    task = Process(
+        name=name,
+        eventFinish=Event(),
+        eventStart=Event(),
+        priority=priority,
+        time=time,
+        resources=resources
+    )
+    task.thread = Thread(
+        target=target,
+        args=(name, task.eventStart, task.eventFinish, time)
+    )
+    return task
