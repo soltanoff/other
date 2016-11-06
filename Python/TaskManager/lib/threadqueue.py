9f1dece98d3a9d75a80ffec03b5237be6e5386c3
Ilya <piccadillable@gmail.com>
2016-11-06 18:57:03 +0300

    Remove "Средний" priority, added ownership persent
    RESOURCES_OWNERSHIP_PERSENT - needed for up task priority when task have
    more resources than all system avialable.

--- Python/TaskManager/lib/threadqueue.py
+++ Python/TaskManager/lib/threadqueue.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 # from config import TASK_STATUS
+from config import RESOURCES_LIST, RESOURCES_OWNERSHIP_PERSENT
 
 
 class CThreadQueue(object):
@@ -22,16 +23,34 @@ class CThreadQueue(object):
         self.pos = 1
         self.avgTime = 0
 
+    def countOfActive(self):
+        count = 0
+        for x in self.data:
+            if x.status not in [4]:
+                count += 1
+        return count
+
     def push(self, x):
+        if self.countOfActive():
+            x.status = 3
         self.data.append(x)
+
+    def checkResources(self, element):
+        for x in self.data:
+            if x.processor != '- none -' and x.status in [2, 3]:
+                for y in element.resources:
+                    if y in x.resources:
+                        return False
+        return True
 
     def pop(self):
         index = 0
         if self.data:
             while index < len(self.data):
-                if self.data[index].status not in [4, 2]:
-                    self.data[index].status = 2
-                    return self.data[index]
+                if self.data[index].status not in [4, 2] and self.data[index].processor == '- none -':
+                    if self.checkResources(self.data[index]):
+                        self.data[index].status = 2
+                        return self.data[index]
                 index += 1
             return None
         else:
@@ -49,15 +68,30 @@ class CThreadQueue(object):
 
     def last(self):
         for x in range(-1, -len(self.data), -1):
-            if self.data[x].status == 3:
-                return self.data[x]
+            if self.data[x].status == 3 and self.data[x].processor == '- none -':
+                if self.checkResources(self.data[x]):
+                    return self.data[x]
         return None
+
+    def reviewPriority(self):
+        index = 0
+        if self.data:
+            while index < len(self.data):
+                if len(self.data[index].resources) > RESOURCES_OWNERSHIP_PERSENT * len(RESOURCES_LIST) and \
+                                self.data[index].priority != 0:
+                    self.data[index].priority = 0
+                index += 1
+
+    # sorting (realize planing)
+    def sort(self):
+        self.data.sort(key=lambda k: (k.status, k.priority, k.time, -len(k.resources)))
 
     # SJF sorting
     def startPlanning(self):
-        self.data.sort(key=lambda k: (k.status, k.priority, k.time, -len(k.resources)))
+        self.sort()
+        self.reviewPriority()
         self.changeStatus()
-        self.data.sort(key=lambda k: (k.status, k.priority, k.time, -len(k.resources)))
+        self.sort()
         self.calculateAvgTime()
 
     def changeStatus(self, checkFinished=False):
